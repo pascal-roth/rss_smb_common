ff8b5d04c66f57392cb8164d0d38a7a54afabfbd
TianyiZhang-arc <70370246+TianyiZhang-arc@users.noreply.github.com>
2022-07-07 00:06:31 +0200

    Add files via upload
    object detection filter

---
+++ output_result.py
@@ -0,0 +1,68 @@
+import pandas as pd
+import numpy as np
+import glob
+
+# objects
+# 25 backpack
+# 26 umbrella
+# 40 bottle
+# 12 stop sign
+# 57 chair
+# 75 clock
+
+# params
+# DIST_THRES = {
+#     'backpack': 3,
+#     'umbrella': 7,
+#     'bottle': 1,
+#     'stop sign': 5,
+#     'chair': 10,
+#     'clock': 5
+# }
+DIST_THRES = {
+    'bicycle': 1,
+    'person': 1,
+    'stop sign': 1
+}
+
+
+
+dirname = '/home/zty/Desktop/detection_data/'
+# objects_name = ['backpack', 'umbrella', 'bottle', 'stop sign', 'chair', 'clock'] # note the stop sign
+objects_name = ['bicycle', 'stop sign', 'person']
+fields = ['Timestamp', 'Confidence', 'Position x', 'Position y', 'Position z', 'Label']
+result = {}
+for key in objects_name:
+    result[key] = []    # loc, confidence
+
+filenames = glob.glob(dirname + '*.csv')
+# print(filenames)
+for filename in filenames:
+    df = pd.read_csv(filename)
+    for obj_name in objects_name:
+        same_objs = df.loc[df['Label'] == obj_name]
+        for i, row in same_objs.iterrows():
+            loc = np.array([row['Position x'], row['Position y'], row['Position z']])
+            if len(result[obj_name]) == 0:
+                result[obj_name].append([loc, row['Confidence']])
+                # print(f'generate new object of {obj_name}, num: {len(result[obj_name])}')
+            else:
+                renew_flag = 0
+                for j in range(len(result[obj_name])):
+                    if np.linalg.norm(loc - result[obj_name][j][0]) < (DIST_THRES[obj_name]**2):
+                        renew_flag = 1
+                        if row['Confidence'] > result[obj_name][j][1]:
+                            result[obj_name][j][0] = loc
+                            result[obj_name][j][1] = row['Confidence']
+                            # print(f'renew location of {obj_name}')
+                            
+                if renew_flag == 0:
+                    # if obj_name == 'bicycle':
+                        # print(result[obj_name])
+                    result[obj_name].append([loc, row['Confidence']])
+                    print(f'generate new object of {obj_name}, num: {len(result[obj_name])}')
+# print(result)
+
+for key in objects_name:
+    print(key, len(result[key]))
+    
